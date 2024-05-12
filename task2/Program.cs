e017103b0bf1085f16a85aa57c911358733ec3af
Vladimir <vlnshapat@gmail.com>
2024-05-12 21:21:46 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//вывод на экран квадратов чисел от 0 до N.
+
+void PrintSqure(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+
+PrintSqure(22);
