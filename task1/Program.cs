9a69cee003c62e7da07e1f55034ee5f7e9459e7c
Vladimir <vlnshapat@gmail.com>
2024-05-12 20:55:27 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//вычислить  значение формолы  (a*b)/(c+d)
+//решение оформить в виде формулы
+
+
+double CalculFormula(int a, int b, int c, int d)
+{
+    double num1 = a * b;
+    int num2 = c + d;
+    double result = num1 / num2;
+    return result;
+}
+
+double result = CalculFormula(5, 78, 45, 25);
+Console.WriteLine(result);
