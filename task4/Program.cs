e689f01eff00511ed3dd091e70d9682e8ae48189
Vladimir <vlnshapat@gmail.com>
2024-05-13 00:09:15 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿//заполнение массива случайными числами от  1 до 9 включительно
+// вывод на консоль их
+
+Random rnd = new Random();
+
+int size = 940;
+int[] array = new int[size];
+
+int i = 0;
+while (i < size)
+{
+    array[i] = rnd.Next(1, 95);
+    i++;
+}
+
+i = 0;
+while (i < size)
+{
+    Console.Write($"{array[i]} ");
+    i++;
+  }
