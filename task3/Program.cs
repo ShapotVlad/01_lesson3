81e6798351765a4ad4351073ee6edbe78e033f07
Vladimir <vlnshapat@gmail.com>
2024-05-12 23:22:25 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// обнулить четные  значения массива
+
+
+
+void ZeroEvenElements(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            numbers[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    foreach (int e in numbers)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 9, 8, 12, 5, 13, 4, 7 };
+ZeroEvenElements(array);
+PrintArray(array);
