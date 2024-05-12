f5e7d304b62d92055050355d07be556adc9d7148
Vladimir <vlnshapat@gmail.com>
2024-05-13 00:34:21 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,15 @@
+﻿//ввод данных с консоли
+
+
+int size = 13;
+int[] array = new int[size];
+int i = 0;
+
+while (i < size)
+{
+    Console.WriteLine("введите целое число массива:");
+
+    string input = Console.ReadLine();
+    array[i] = Convert.ToInt32(input);
+    i++;
+}
