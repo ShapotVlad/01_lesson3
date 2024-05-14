33b71b1d5773d35d7512ee93a6f1f3806b6be7ae
Vladimir <vlnshapat@gmail.com>
2024-05-14 10:59:33 +0300

    add commit

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,14 +1,14 @@
 ﻿//вычислить  значение формолы  (a*b)/(c+d)
-//решение оформить в виде формулы
+//решение оформить в виде функции
 
 
-double CalculFormula(int a, int b, int c, int d)
+double CalculFormula(int a, int b, int c, int d) //создали функцию
 {
-    double num1 = a * b;
+    double num1 = a * b;                         // тело функции, что она делает 
     int num2 = c + d;
     double result = num1 / num2;
-    return result;
+    return result;                              // возврат  функции
 }
 
-double result = CalculFormula(5, 78, 45, 25);
-Console.WriteLine(result);
+double result = CalculFormula(45, 78, 45, 25);  //вызов функции
+Console.WriteLine(result);                     // вывод результата 
