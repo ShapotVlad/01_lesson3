d688a31b0f7e3852933b30a8bfd36cf8ce58ffb3
VladShapot <vlnshapot@gmail.com>
2024-05-31 12:04:50 +0300

    добавили изменения

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,13 +2,13 @@
 //решение оформить в виде функции
 
 
-double CalculFormula(int a, int b, int c, int d) //создали функцию
+double CalculFormula(int a, int b, int c, int d) //создали локальную возвратную функцию
 {
     double num1 = a * b;                         // тело функции, что она делает 
     int num2 = c + d;
     double result = num1 / num2;
-    return result;                              // возврат  функции
+    return result;                              // возвращаем результат   функции для дальнейшего пользования
 }
 
-double result = CalculFormula(45, 78, 45, 25);  //вызов функции
+double result = CalculFormula(45, 78, 45, 25);  //вызовваем локальную функцию и вводим параметры
 Console.WriteLine(result);                     // вывод результата 
